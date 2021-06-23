11bc68c6f251604af20292e5f39eec03bd1acec0
halel-sarusi <halels1401@gmail.com>
2021-06-23 22:05:21 +0300

    Adding a drawer, page design

--- screens/MyReportsScreen.js
+++ screens/MyReportsScreen.js
@@ -13,8 +13,10 @@ import {
 import { useDispatch } from 'react-redux';
 import * as reportActions from '../store/action/report';
 import Colors from '../constants/Colors';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import ReportItem from '../components/ReportItem';
+import { Card } from 'react-native-paper';
+
 
 const MyReportsScreen = (props) => {
   const dispatch = useDispatch();
@@ -23,12 +25,12 @@ const MyReportsScreen = (props) => {
   const reports = useSelector(state => state.report.reportsArr);
 
 
-  const loadReports = useCallback(async() => {
+  const loadReports = useCallback(async () => {
     setError(null);
     setIsLoading(true);
-    try{
+    try {
       await dispatch(reportActions.fetchReports());
-    } catch(err) {
+    } catch (err) {
       setError(err.message)
     }
     setIsLoading(false);
@@ -38,54 +40,72 @@ const MyReportsScreen = (props) => {
     loadReports();
   }, [dispatch, loadReports]);
 
-  if(error){
-    return(
-      <View style={{flex:1, justifyContent:'center'}}>
+  if (error) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
         <Text>An error occured!</Text>
-        <Button 
-          title="Try Again" 
-          onPress={loadReports} 
+        <Button
+          title="Try Again"
+          onPress={loadReports}
           color={Colors.primary}
         />
-    </View>
-    )
-  }
-
-  if(isLoading){
-    return(
-      <View style={{flex:1, justifyContent:'center'}}>
-        <ActivityIndicator size='large' color={Colors.primary}/>
       </View>
     )
   }
 
-  if(!isLoading && reports?.length === 0){
-    return(
-      <View style={{flex:1, justifyContent:'center'}}>
-      <Text>אין דיווחים קודמים!</Text>
-    </View>
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <ActivityIndicator size='large' color={Colors.primary} />
+      </View>
     )
   }
 
-  return(
-    <FlatList 
-      data={reports}
-      keyExtractor={item => item.id}
-      renderItem={itemData => (
-        <ReportItem 
-          image={itemData.item.imageUrl}
-          address={itemData.item.address}
-          desc={itemData.item.desc}
-          onSelect={() => {
-            props.navigation.navigate('ReportDetails', {
-              report: itemData.item,
-            });
-          }}
-        />
-      )}
-    />
+  if (!isLoading && reports?.length === 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>אין דיווחים קודמים!</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={{ alignItems: 'center' }}>
+      <View style={{ backgroundColor: 'lightskyblue', width: '100%', height: 100 }}>
+        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center', marginTop: 40, color: 'white' }}>דיווחים קודמים</Text>
+      </View>
+      <Card style={styles.card}>
+      <FlatList
+        data={reports}
+        keyExtractor={item => item.id}
+        renderItem={itemData => (
+          <ReportItem
+            image={itemData.item.imageUrl}
+            address={itemData.item.address}
+            desc={itemData.item.desc}
+            onSelect={() => {
+              props.navigation.navigate('ReportDetails', {
+                report: itemData.item,
+              });
+            }}
+          />
+        )}
+      />
+      </Card>
+    </View>
   )
 }
 
-
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white', 
+    height: '85%', 
+    width: '95%',
+    borderRadius: 30,
+    marginTop: 20,
+  },
+  ocard: {
+    backgroundColor: 'red',
+  }
+});
 export default MyReportsScreen;
