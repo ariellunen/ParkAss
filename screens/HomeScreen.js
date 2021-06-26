759b99e6af0524b52c736fc4df29aedc71bca800
Ariel <ariellunen@gmail.com>
2021-06-26 12:41:13 +0300

    whatsApp message working with image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,38 @@
-import { View, Text, Button,Linking, StyleSheet,Alert,Image, ImageBackground, Dimensions  } from 'react-native';
-import { Card, Avatar, IconButton } from 'react-native-paper';
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import * as authActions from '../store/action/auth';
-import * as ImagePicker from 'expo-image-picker';
-import * as reportActions from '../store/action/report';
-
-const LeftContent = props => <Avatar.Icon {...props} icon="home" backgroundColor="lightskyblue" />
+import {
+  View,
+  Text,
+  Button,
+  Linking,
+  StyleSheet,
+  Alert,
+  Image,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+import { Card, Avatar, IconButton } from "react-native-paper";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import * as authActions from "../store/action/auth";
+import * as ImagePicker from "expo-image-picker";
+import * as reportActions from "../store/action/report";
+import firebase from "firebase/app";
+require("firebase/database");
+require("firebase/auth");
+require("firebase/storage");
+const LeftContent = (props) => (
+  <Avatar.Icon {...props} icon="home" backgroundColor="lightskyblue" />
+);
 const HomeScreen = (props) => {
   const { width, height } = Dimensions.get("window");
   const dispatch = useDispatch();
   const [pickedImage, setPickedImage] = useState();
   const verifyPermissions = async () => {
     const result = await ImagePicker.requestCameraPermissionsAsync();
-    if (result.status !== 'granted') {
+    if (result.status !== "granted") {
       Alert.alert(
-        'Insufficient permissions!',
-        'You need to grant camera permissions to use this app.',
-        [{ text: 'Okay' }]
+        "Insufficient permissions!",
+        "You need to grant camera permissions to use this app.",
+        [{ text: "Okay" }]
       );
       return false;
     }
@@ -34,29 +49,65 @@ const HomeScreen = (props) => {
       aspect: [16, 9],
       quality: 0.5,
       base64: true,
-      
     });
 
     // let imageUri = `data:image/jpg;base64,${image.base64}`
 
-    // setPickedImage(imageUri);
-    console.log(<Image source={{uri: image.uri}}/>);
+    setPickedImage(image.uri);
+    console.log(image.uri);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword("email@email.com", "12345678")
+      .catch((error) => {
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+    uploadmultimedia();
     dispatch(reportActions.addImage(image.uri));
+  };
+  const uploadmultimedia = async () => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response); // when BlobModule finishes reading, resolve with the blob
+      };
+      xhr.onerror = function () {
+        reject(new TypeError("Network request failed")); // error occurred, rejecting
+      };
+      xhr.responseType = "blob"; // use BlobModule's UriHandler
+      xhr.open("GET", pickedImage, true); // fetch the blob from uri in async mode
+      xhr.send(null); // no initial data
+    });
 
-    // Linking.openURL(`http://api.whatsapp.com/send?phone=9720542344828&text=${image.uri}`)
+    var timestamp = new Date().getTime();
+    var imageRef = firebase.storage().ref(`users/Dp/` + timestamp + "/");
 
-
-    // props.navigation.navigate('Map');
+    return imageRef
+      .put(blob)
+      .then(() => {
+        blob.close();
+        return imageRef.getDownloadURL();
+      })
+      .then((dwnldurl) => {
+        dispatch(reportActions.addImage(dwnldurl));
+        props.navigation.navigate('Map');
+      });
   };
 
-
   return (
-    <ImageBackground source={{ uri: 'https://i.postimg.cc/J7P1jYZk/image.jpg' }} style={{ width: '100%', height: '100%', alignItems: 'center' }}>
+    <ImageBackground
+      source={{ uri: "https://i.postimg.cc/J7P1jYZk/image.jpg" }}
+      style={{ width: "100%", height: "100%", alignItems: "center" }}
+    >
       <Card style={styles.card} dir="rtl">
-        <Card.Title title="מסך הבית" subtitle="בחר את הפעולה המתאימה" left={LeftContent} />
+        <Card.Title
+          title="מסך הבית"
+          subtitle="בחר את הפעולה המתאימה"
+          left={LeftContent}
+        />
         <View style={styles.all}>
           <View style={styles.SingleCard}>
-            <View style={{ backgroundColor: 'darkseagreen', borderRadius: 70 }}>
+            <View style={{ backgroundColor: "darkseagreen", borderRadius: 70 }}>
               <IconButton
                 icon="camera"
                 size={50}
@@ -68,38 +119,42 @@ const HomeScreen = (props) => {
             <Text style={styles.paragraph}>מצלמה</Text>
           </View>
           <View style={styles.SingleCard}>
-            <View style={{ backgroundColor: 'lightcoral', borderRadius: 70 }}>
+            <View style={{ backgroundColor: "lightcoral", borderRadius: 70 }}>
               <IconButton
                 icon="file"
                 size={50}
                 color="white"
                 title="My Reports"
-                onPress={() => { props.navigation.navigate('Reports'); }
-                } />
+                onPress={() => {
+                  props.navigation.navigate("Reports");
+                }}
+              />
             </View>
             <Text style={styles.paragraph}>דיווחים קודמים</Text>
           </View>
           <View style={styles.SingleCard}>
-            <View style={{ backgroundColor: 'lightblue', borderRadius: 70 }}>
+            <View style={{ backgroundColor: "lightblue", borderRadius: 70 }}>
               <IconButton
                 icon="logout"
                 size={50}
                 color="white"
                 title="Logout"
-                onPress={() => { dispatch(authActions.logout()); }
-                } />
+                onPress={() => {
+                  dispatch(authActions.logout());
+                }}
+              />
             </View>
             <Text style={styles.paragraph}>יציאה</Text>
           </View>
           <View style={styles.SingleCard}>
-            <View style={{ backgroundColor: 'thistle', borderRadius: 70 }}>
+            <View style={{ backgroundColor: "thistle", borderRadius: 70 }}>
               <IconButton
                 icon="phone"
                 size={50}
                 color="white"
                 title="Call the police"
-                onPress={() => { }
-                } />
+                onPress={() => {}}
+              />
             </View>
             <Text style={styles.paragraph}>חיוג למשטרה</Text>
           </View>
@@ -107,35 +162,34 @@ const HomeScreen = (props) => {
       </Card>
     </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: 'white',
-    height: '60%',
+    backgroundColor: "white",
+    height: "60%",
     borderRadius: 30,
-    width: '80%',
-    marginTop: 'auto',
-    marginBottom: 'auto',
+    width: "80%",
+    marginTop: "auto",
+    marginBottom: "auto",
   },
   all: {
     marginTop: 50,
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
   },
   SingleCard: {
     height: Dimensions.get("window").height * 0.2,
     width: Dimensions.get("window").width * 0.4,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 30,
   },
   paragraph: {
     fontSize: 15,
-    color: 'black',
-    textAlign: 'center',
+    color: "black",
+    textAlign: "center",
   },
 });
 
 export default HomeScreen;
-
